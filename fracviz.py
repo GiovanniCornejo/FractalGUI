754e1aa56f0affa951d2325c39f0085dbef24f35
GiovanniCornejo <giocornejo424@gmail.com>
2024-04-08 14:46:17 -0400

    Added documentation and reset button signal/slot

--- fracviz.py
+++ fracviz.py
@@ -1,5 +1,5 @@
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtCore import QFile, QIODevice, QObject
+from PySide2.QtCore import QFile, QIODevice, QObject, Signal, Slot
 from PySide2.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QPushButton, QLabel
 
 from matplotlib import pyplot
@@ -17,21 +17,44 @@ class FractalApp(QObject):
     """
 
     def __init__(self, file: str):
-        # Instantiate fractalwindow from file
+        """
+        Constructor for the FractalApp class. The file is used to create the FractalWindow widget.
+        This is also responsible for instantiating the Mandelbrot set class using default values from the GUI,
+        updating the plotting image, and displaying it.
+        
+        Parameters:
+        file: The relative path to the .UI file to load.
+        """
+        super().__init__()
         self.root_widget = FractalWindow(file, self) 
 
     def update_plot(self):
+        """
+        Updates the AxesImage instance. It is responsible for ensuring the display is updated
+        without locking up the application by creating and starting a non-GUI thread.
+        """
+
+        # TODO: Create and start a non-GUI thread for image processing
+
         pass
+
+    def reset_button_clicked(self):
+        print("Reset button clicked")
 
 
 class FractalWindow(QWidget):
     """
     Derives from QWidget and is responsible for setting up and connecting elements of the GUI.
     """
+
     def __init__(self, file: str, app: FractalApp):
         """
-        Constructor for the FractalWindow class. The file is used to load the UI from the file. 
+        Constructor for the FractalWindow class. The file is used to load the UI. 
         This is also responsible for preparing all elements of the GUI.
+
+        Parameters:
+        file: The relative path to the .UI file to load.
+        app: A reference to the FractalApp instance which called the constructor
         """
         super().__init__()
 
@@ -62,13 +85,14 @@ class FractalWindow(QWidget):
         self.figure = subplot[0]
         self.axes: Axes = subplot[1]
         self.canvas = FigureCanvas(self.figure)
-
-        # Adding the FigureCanvas object as a child of the layout instance
         self.layout.addWidget(self.canvas)
 
         # TODO: Link widget signals to slots (methods/functions)
+        self.reset_button.clicked.connect(app.reset_button_clicked)
+
 
     def get_child(self, parent, widget_type, widget_name):
+        """Helper function for retrieving child widgets"""
         widget = parent.findChild(widget_type, widget_name)
         if isinstance(widget, widget_type):
             return widget
